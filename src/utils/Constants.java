0f84184e18f9ecd6469ba43353623b46b59e6cdb
s308597 <gabrieledem36@gmail.com>
2025-04-03 13:03:44 +0100

    Add Sprite

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
     public static final int BOARD_WIDTH = 640;
     public static final int BOARD_HEIGHT = 480;
 
-    public static final String PLAYER_IMAGE_PATH = "resources/amogus.png";
+    public static final String PLAYER_IMAGE_PATH = "resources/Where ma hug @ Twin.jpg";
     public static final int PLAYER_WIDTH = 60;
     public static final int PLAYER_HEIGHT = 80;
     public static final int PLAYER_SPEED = 10;
